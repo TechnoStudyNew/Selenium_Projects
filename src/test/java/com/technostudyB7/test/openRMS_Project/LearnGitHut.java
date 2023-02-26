4bc601ce35175ab4c2204a4342907227c7ac9344
canny <canny_02@hotmail.com>
2023-02-25 21:09:59 -0500

    i creaded new one

--- src/test/java/com/technostudyB7/test/openRMS_Project/LearnGitHut.java
+++ src/test/java/com/technostudyB7/test/openRMS_Project/LearnGitHut.java
@@ -3,6 +3,7 @@ package com.technostudyB7.test.openRMS_Project;
 public class LearnGitHut {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println("Hello again");
     }
 
 }
