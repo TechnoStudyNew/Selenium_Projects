aae16993325907928cd39b62c16396215e682e4c
Eray Sahin <eraysahinmoon@gmail.com>
2023-02-25 20:42:12 -0500

    initial commit

--- src/test/java/com/technostudyB7/test/openRMS_Project/learnGithub.java
+++ src/test/java/com/technostudyB7/test/openRMS_Project/learnGithub.java
@@ -2,8 +2,6 @@ package com.technostudyB7.test.openRMS_Project;
 
 public class learnGithub {
     public static void main(String[] args) {
-        System.out.println("Hello Github");
-        System.out.println("New Moon==>Ayse");
-
+        System.out.println("Hello GitHub");
     }
 }
