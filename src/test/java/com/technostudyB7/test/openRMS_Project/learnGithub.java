885b749bb6092556f4f5156eb2111de62203556a
Eray Sahin <eraysahinmoon@gmail.com>
2023-02-25 21:05:34 -0500

    added new line

--- src/test/java/com/technostudyB7/test/openRMS_Project/learnGithub.java
+++ src/test/java/com/technostudyB7/test/openRMS_Project/learnGithub.java
@@ -2,6 +2,7 @@ package com.technostudyB7.test.openRMS_Project;
 
 public class learnGithub {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub");
+        System.out.println("Hey GitHub");
+        System.out.println("This is Eray.");
     }
 }
