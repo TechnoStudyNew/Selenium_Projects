2012fb7692bec8b0f443bbef9e08db8552270e3a
Eray Sahin <eraysahinmoon@gmail.com>
2023-02-25 21:35:06 -0500

    edited new line

--- src/test/java/com/technostudyB7/test/openRMS_Project/learnGithub.java
+++ src/test/java/com/technostudyB7/test/openRMS_Project/learnGithub.java
@@ -2,7 +2,8 @@ package com.technostudyB7.test.openRMS_Project;
 
 public class learnGithub {
     public static void main(String[] args) {
-        System.out.println("Hey GitHub");
-        System.out.println("This is Eray.");
+        System.out.println("Hello Github");
+        System.out.println("Hi! I am Eray.");
+
     }
 }
