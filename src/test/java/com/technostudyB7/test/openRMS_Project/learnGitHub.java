cabb2c7ba4650a9803dd080689a9b7fe6b3eab40
IpekYurtbilen <ipekkeskek@yahoo.com>
2023-02-25 20:00:34 -0600

    I have created a new code

---
+++ src/test/java/com/technostudyB7/test/openRMS_Project/learnGitHub.java
@@ -0,0 +1,9 @@
+package com.technostudyB7.test.openRMS_Project;
+
+public class learnGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("Techno Study");
+
+    }
+}
